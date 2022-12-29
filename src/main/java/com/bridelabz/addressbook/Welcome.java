8c20396c2006df40c46d6045681918940ef2d167
sharad2512 <shnarute95@gmail.com>
2022-12-29 13:50:09 +0530

    ADDED : Welcome Message to address book program

---
+++ src/main/java/com/bridelabz/addressbook/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridelabz.addressbook;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome To the Address Book Program");
+    }
+}
